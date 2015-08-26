8b5bb1de3e2f02968fd4f5204aee70f1c861415a
ALEJANDROJ19 <ALEJANDROJ19@hotmail.es>
2015-08-26 22:11:07 +0200

    BUGFIX: utf-8 encode message crash when sending a utf-8 text

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ class WebhookHandler(webapp2.RequestHandler):
             if msg:
                 resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
                     'chat_id': str(chat_id),
-                    'text': msg,
+                    'text': msg.encode('utf-8'),
                     'disable_web_page_preview': 'true',
                 })).read()
             else:
@@ -148,7 +148,7 @@ class WebhookHandler(webapp2.RequestHandler):
             if msg:
                 resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
                     'chat_id': str(chat_id),
-                    'text': msg,
+                    'text': msg.encode('utf-8'),
                     'disable_web_page_preview': 'true',
                     'reply_to_message_id': str(message_id),
                 })).read()
